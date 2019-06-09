314fc1fc688797c86fc243cc74e61afe530211a2
dev.holman <devyn.holman@yahoo.com>
2019-06-08 22:07:38 -0500

    initial project structure setup with bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
+const url = 'https://demo.iofficeconnect.com/external/api/rest/v2/users';
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'x-auth-username': 'username',
+    'x-auth-password': 'password'
+  }),
+  params: new HttpParams().set('limit', '100')
+};
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'iOffice-directory';
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.onFetchUsers();
+  }
+
+  onFetchUsers() {
+    this.http
+      .get(
+        'https://demo.iofficeconnect.com/external/api/rest/v2/users',
+        httpOptions
+      )
+      .subscribe(res => {
+        console.log(res);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { HttpClientModule } from '@angular/common/http';
+import { UsersComponent } from './users/users.component';
+import { UsersListComponent } from './users/users-list/users-list.component';
+import { UserSearchComponent } from './users/user-search/user-search.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { UserCardComponent } from './users/users-list/user-card/user-card.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeaderComponent, UsersComponent, UsersListComponent, UserSearchComponent, SidebarComponent, UserCardComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
